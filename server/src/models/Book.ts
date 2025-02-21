d0195a995e0ac5a6f0d657cf87642997233ac771
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-02-20 18:00:50 -0600

    Completed Book model

---
+++ server/src/models/Book.ts
@@ -0,0 +1,58 @@
+//imports
+import { Schema, model, type Document } from "mongoose";
+
+//interface
+export interface BookDocument extends Document {
+    bookId: string;
+    title: string;
+    authors?: string[];
+    description?: string;
+    thumbnail?: string;
+    pageCount?: number;
+    categories?: string;
+    averageRating?: number;
+    ratingsCount?: number;
+    infoLink?: string;
+}
+
+//bookSchema - Part of User's array as subdocument
+const bookSchema = new Schema<BookDocument>({
+    bookId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    authors: [
+        {
+            type: String
+        }
+    ],
+    description: {
+        type: String
+    },
+    thumbnail: {
+        type: String
+    },
+    pageCount: {
+        type: Number,
+    },
+    categories: {
+        type: String
+    },
+    averageRating: {
+        type: Number
+    },
+    ratingsCount: {
+        type: Number
+    },
+    infoLink: {
+        type: String
+    }
+})
+
+//export bookSchema
+const Book = model<BookDocument>('Book', bookSchema);
+export default Book;
